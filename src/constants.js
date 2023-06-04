949242713d7ceb91c74a4baa3fc33c53741e447d
rafaelalma <rafaelalma92@gmail.com>
2023-06-04 18:08:14 +0200

    set up breakpoints

--- src/constants.js
+++ src/constants.js
@@ -9,10 +9,23 @@ export const COLORS = {
   },
   primary: 'hsl(340deg 65% 47%)',
   secondary: 'hsl(240deg 60% 63%)',
-};
+}
 
 export const WEIGHTS = {
   normal: 500,
   medium: 600,
   bold: 800,
-};
+}
+
+// Desktop-first
+const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 950,
+  laptopMax: 1300,
+}
+
+export const QUERIES = {
+  phoneAndDown: `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+  tabletAndDown: `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+  laptopAndDown: `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+}
